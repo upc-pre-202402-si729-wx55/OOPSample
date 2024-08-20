98131f91901e2d2e2dd65d852c6d6191c9d749a1
Angel Velasquez <pcisavel@upc.edu.pe>
2024-08-19 20:42:34 -0500

    feat: added state and behavior to address value object.

--- src/main/java/com/acme/shared/domain/model/valueobjects/Address.java
+++ src/main/java/com/acme/shared/domain/model/valueobjects/Address.java
@@ -2,4 +2,32 @@ package com.acme.shared.domain.model.valueobjects;
 
 public class Address {
     private String street;
+    private String number;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String country;
+
+    public Address() {
+        this.street = "";
+        this.number = "";
+        this.city = "";
+        this.state = "";
+        this.zipCode = "";
+        this.country = "";
+    }
+
+
+    public Address(String street, String number, String city, String state, String zipCode, String country) {
+        this.street = street;
+        this.number = number;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.country = country;
+    }
+
+    public String getAddressAsString() {
+        return String.format("%s %s, %s, %s, %s, %s", street, number, city, state, zipCode, country);
+    }
 }
